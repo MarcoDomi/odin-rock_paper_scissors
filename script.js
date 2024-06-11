aff1f351f29f170988156d3531846d87bb30f718
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-06-11 18:39:56 -0500

    include event listeners in playgame function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-//let humanScore = 0;
-//let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
@@ -9,8 +9,7 @@ function getComputerChoice() {
     return choices[computerChoice];
 }
 
-function getHumanChoice() {
-    const choice = prompt('Enter rock, paper or scissors: ');
+function getHumanChoice(choice) {
 
     switch (choice.toLowerCase()) {
         case 'rock':
@@ -22,57 +21,33 @@ function getHumanChoice() {
     };
 }
 
-/*
 function playRound(humanChoice, computerChoice) {
-    
-    if(humanChoice === computerChoice) {
-            console.log('Draw!')
+    if (humanChoice === computerChoice) {
+           console.log('Draw!')
     }
-    else if((humanChoice === 'scissors' && computerChoice === 'paper') ||
-            (humanChoice === 'paper' && computerChoice === 'rock') ||
-            (humanChoice === 'rock' && computerChoice === 'scissors')){
-            
-            humanScore++;
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`); 
+    else if ((humanChoice === 'scissors' && computerChoice === 'paper') ||
+             (humanChoice === 'paper' && computerChoice === 'rock') ||
+             (humanChoice === 'rock' && computerChoice === 'scissors')) {
+        
+        humanScore++;
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
     }
     else {
         computerScore++;
         console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-    }
+    } 
 }
-*/
-
 
 function playGame() {
-    const NUMBER_OF_ROUNDS = 5;
-    let humanScore = 0;
-    let computerScore = 0;
+    btns = document.querySelectorAll('button');
 
-    let playRound = (humanChoice, computerChoice) => {
-        if (humanChoice === computerChoice) {
-            console.log('Draw!')
-        }
-        else if ((humanChoice === 'scissors' && computerChoice === 'paper') ||
-            (humanChoice === 'paper' && computerChoice === 'rock') ||
-            (humanChoice === 'rock' && computerChoice === 'scissors')) {
-            
-            humanScore++;
-            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        }
-        else {
-            computerScore++;
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-        }
-    };
-
-    for (let i = 0; i < NUMBER_OF_ROUNDS; i++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        console.log(humanSelection);
-        console.log(computerSelection);
-        playRound(humanSelection, computerSelection);
-    }
-
+    btns.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            let humanChoice = btn.innerText.toLocaleLowerCase();
+            let computerChoice = getComputerChoice();
+            playRound(humanChoice, computerChoice);
+        });
+    });
 }
 
 playGame();
