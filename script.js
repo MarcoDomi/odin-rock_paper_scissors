df046ce88477bd09af18e72ac5d16f48f35ec1f2
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-06-11 19:51:09 -0500

    finish game ui

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 let humanScore = 0;
 let computerScore = 0;
 
+let humanScoreSpan = document.querySelector('#humanScore');
+let computerScoreSpan = document.querySelector('#computerScore');
+let resultMsg = document.querySelector('#gameResult');
+
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     const computerChoice = Math.floor(Math.random() * 3);
@@ -9,33 +13,45 @@ function getComputerChoice() {
     return choices[computerChoice];
 }
 
-function getHumanChoice(choice) {
-
-    switch (choice.toLowerCase()) {
-        case 'rock':
-        case 'paper':
-        case 'scissors':
-            return choice.toLocaleLowerCase();
-        default:
-            return null;
-    };
+function updatePageScore(span, newScore) {
+    let spanMessage = span.innerText;
+    span.innerText = spanMessage.slice(0, -1) + newScore;
 }
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
-           console.log('Draw!')
+        resultMsg.innerText = 'Draw!'
     }
     else if ((humanChoice === 'scissors' && computerChoice === 'paper') ||
              (humanChoice === 'paper' && computerChoice === 'rock') ||
              (humanChoice === 'rock' && computerChoice === 'scissors')) {
         
         humanScore++;
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        updatePageScore(humanScoreSpan, humanScore);
+        resultMsg.innerText = `You win! ${humanChoice} beats ${computerChoice}`;
     }
     else {
         computerScore++;
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+        updatePageScore(computerScoreSpan, computerScore);
+        resultMsg.innerText = `You lose! ${computerChoice} beats ${humanChoice}`;
     } 
+}
+
+function checkWinner() {
+
+    if (humanScore === 5) {
+        resultMsg.innerText = 'Player wins the game!';
+    }
+    else if (computerScore === 5) {
+        resultMsg.innerText = 'Computer wins the game!';
+    }
+
+    if (humanScore === 5 || computerScore === 5) {
+        humanScore = 0;
+        computerScore = 0;
+        updatePageScore(humanScoreSpan, humanScore);
+        updatePageScore(computerScoreSpan, computerScore);
+    }
 }
 
 function playGame() {
@@ -46,6 +62,8 @@ function playGame() {
             let humanChoice = btn.innerText.toLocaleLowerCase();
             let computerChoice = getComputerChoice();
             playRound(humanChoice, computerChoice);
+            checkWinner();
+
         });
     });
 }
